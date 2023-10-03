0bf4fc34bb78330d1e3759af954cb598b4fd0540
KIMO892004 <kimoragab2004@gmail.com>
2023-10-03 15:44:10 -0700

    k

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# Installing flask from pip3  using Puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
